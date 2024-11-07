b60a21b9decba871d2dc4dcdd74229206342454d
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2024-11-06 22:51:31 -0300

    upload do arquivo de rascunho ainda precisaria de tempo para deixar mais responsivo e mostrar em uma div eu acho

---
+++ Documentação/app/static/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('fileInput').addEventListener('change',function(event){
+    const file = event.target[0];
+    if(file && file.type.startsWith('imag/')){
+        const reader =new FileRender();
+        reader.onload = function(e){
+            const preview = document.getElementById('preview');
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert('por favor, selecione um arquivo');
+    }
+    
+});
